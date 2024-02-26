b15b753c19dec01906950045c8a67c879d45559f
Ahmed Mahmoud <104006521+AhMmedMahmoud@users.noreply.github.com>
2024-02-26 13:56:26 +0200

    Add files via upload

---
+++ V2X_interfaces/VDP_Proxy.h
@@ -0,0 +1,108 @@
+#ifndef VDP_PROXY_H     
+#define VDP_PROXY_H   
+
+#include "../someip/service/event_Only/event_service_requester.h"
+#include "../helper/instance_id.h"
+
+using namespace ara::com::someip::sd;
+using namespace ara::com::helper;
+using namespace AsyncBsdSocketLib;
+
+
+namespace ara 
+{
+    namespace com
+    {
+        namespace proxy
+        {    
+            template <typename t>
+            using ServiceHandleContainer = std::vector<t>;
+
+            class VDP_Proxy
+            {
+                public:
+                    using Handler = std::function<void(const std::vector<uint8_t> &)>;
+
+                    class HandleType
+                    {
+                        private:  
+                            /********************** attributes ************************/
+                         
+                            ara::com::InstanceIdentifier identifier;
+                            int16_t port_no;
+                            EventServiceRequester *requester;
+
+                        
+                        public:
+                            /******************** constructors ************************/
+                        
+                            HandleType(InstanceIdentifier id,int16_t p);
+
+
+                            /************ setters and getters *************************/
+
+                            void setId(InstanceIdentifier id);
+
+                            void setPort(int16_t p);
+
+                            int16_t getPort()const;
+
+                            const InstanceIdentifier &GetInstanceId()const;
+
+                            void setRequester(EventServiceRequester *r);
+
+                            EventServiceRequester* getRequester();  
+                    };
+
+
+                    /*********************** static functions **********************/
+
+                    static ServiceHandleContainer<HandleType> findSerivce(InstanceIdentifier id);
+
+
+                    /************************* constructor **************************/
+
+                    VDP_Proxy (HandleType &handle);
+   
+
+                    /************************ fundemental funtions *******************/
+
+                    void subscribe(size_t maxSampleCount);
+ 
+                    helper::SubscriptionState GetSubscriptionState() const;
+
+                    std::future<bool> setter(std::vector<uint8_t> data);
+
+                    std::future<bool> getter(std::vector<uint8_t> &data);
+
+                    void requestGetting();
+
+                    void printSubscriptionState();
+                    
+                    //bool isSubscribed(int duration);
+
+                    void SetReceiveHandler(Handler h);
+
+                    void UnsetReceiveHandler();
+
+
+                    /******** disable copy constructor and equal assigment operator *****/
+                    
+                    VDP_Proxy(const VDP_Proxy &other) = delete;
+                    VDP_Proxy& operator=(const VDP_Proxy &other) = delete;
+
+
+                    /********************* deconstructor *******************/
+                    
+                    ~VDP_Proxy();
+
+                private:
+                    /*************************** atttibutes ****************/
+
+                    EventServiceRequester *requester;
+            };
+        }
+    }
+}
+
+#endif
